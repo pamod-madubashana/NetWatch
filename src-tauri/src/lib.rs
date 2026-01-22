95309f2bd79232fc3c6ce62f0b7e408e1db7d85b
P A M O D <pamod.main@gmail.com>
2026-01-22 23:42:50 +0530

    Configure tracing subscriber to enable console logging

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ mod utils;
 use commands::connections::get_connections;
 use commands::export::export_connections;
 use utils::logger::{log_debug, log_info, log_warn, log_error};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -16,6 +17,10 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, get_connections, export_connections, log_debug, log_info, log_warn, log_error])
