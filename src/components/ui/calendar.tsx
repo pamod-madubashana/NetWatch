3fb966c2918808fc96b91273dcdf715179533e14
P A M O D <pamod.main@gmail.com>
2026-01-22 22:50:04 +0530

    Fix duplicate parameter name in calendar.tsx IconRight component

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -43,7 +43,7 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
       }}
       components={{
         IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
+        IconRight: ({ ...props2 }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
