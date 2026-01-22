22fd982f1cf02fdbae74e66c70cdb5c6297e2e23
P A M O D <pamod.main@gmail.com>
2026-01-22 22:56:21 +0530

    Remove unused parameters in calendar.tsx Icon components

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -42,8 +42,8 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props2 }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
